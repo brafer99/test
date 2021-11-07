74bf5d05f4fed85340fc10025f9c5ce68076a0b5
Brayan Fernando <brayanfernando02.99@gmail.com>
2021-11-07 01:07:22 -0500

    34:50

---
+++ nosotros.php
@@ -0,0 +1,9 @@
+<?php include("template/cabecera.php") ?>
+
+            <div class="jumbotron">
+                <h1 class="display-3">NOSOTROS</h1>
+                <p class="lead">Desarrollo de sitio</p>
+                <hr class="my-2">
+            </div>
+
+<?php include("template/pie.php") ?>
